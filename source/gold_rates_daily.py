05e25d554b3f0cc50d6854f359a3972494697a38
Satishkumar Sahu <satishshahu2@gmail.com>
2026-04-05 15:18:14 +0530

    issue#8: fix for issue#8
    * issue#8: fix for chat id appears to null
    
    * issue#8: chat_id must be in str
    
    * issue#8: fix for chat_id evals to null
    
    * issue#8: added better logging
    
    * issue#8: updating gold_price_alerts yaml file with correct variable name

--- source/gold_rates_daily.py
+++ source/gold_rates_daily.py
@@ -1,11 +1,38 @@
 import requests
 import os
+import logging
 
-BOT_TOKEN: str = os.getenv("BOT_TOKEN")
-CHAT_ID: str = os.getenv("TELE_CHAT_ID")
-GOLD_API_KEY: str = os.getenv("GOLD_API_KEY")
+# ------------------------------------------
+# Logger setup
+# ------------------------------------------
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
+
+logger = logging.getLogger(__name__)
+
+# ------------------------------------------
+# Env variables
+# ------------------------------------------
+def get_env_variable(var_name: str) -> str:
+    value = os.getenv(var_name)
+    if not value:
+        logger.error(f"Environment variable '{var_name}' is not set.")
+        raise ValueError(f"Environment variable '{var_name}' is required.")
+    return value
+
+
+BOT_TOKEN: str = get_env_variable("BOT_TOKEN")
+CHAT_ID: str = get_env_variable("TELE_CHAT_ID")
+GOLD_API_KEY: str = get_env_variable("GOLD_API_KEY")
 GOLD_API_URL: str = "https://www.goldapi.io/api/XAU/INR"
 TELE_BOT_MSG_URL: str = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+# ------------------------------------------
+# Core functions
+# ------------------------------------------
 
 def get_gold_prices():
     headers = {
@@ -20,7 +47,9 @@ def get_gold_prices():
         price_per_gram_24k = data["price"] / 31.1035 # ounce -> gram
         return round(price_per_gram_24k, 2)
     except requests.exceptions.RequestException as e:
-        print("Error: ", str(e))
+        print("Error fetching gold price: ", str(e))
+        logger.error(f"Error fetching gold price: {str(e)}")
+        raise
 
 def calculate_prices(price_24k):
     price_per_gram_22k = round(price_24k * 0.916, 2)
@@ -28,6 +57,10 @@ def calculate_prices(price_24k):
     return price_per_gram_22k, price_per_gram_18k
 
 def send_telegram(message):
+    logger.info("Sending message to Telegram...")
+    logger.info(f"Message: {message}")
+    logger.info(f"Chat ID: {CHAT_ID}")
+
     requests.post(TELE_BOT_MSG_URL, data={
         "chat_id": CHAT_ID,
         "text": message
